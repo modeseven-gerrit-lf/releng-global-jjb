7f530491195d2d48b6893c6fbc55c51548909b64
Anil Belur <abelur@linuxfoundation.org>
2026-03-17 21:34:17 +1000

    Fix: Add fallback facter not found
    The create-jenkins-user.sh script relies on facter to detect the OS
    and locate the default cloud user's authorized_keys. On Ubuntu 24.04
    images where facter is not installed, OS detection fails silently
    causing the jenkins user's authorized_keys to never be copied. This
    prevents Jenkins from SSH-ing into the node to launch the agent
    process, leaving all jobs using Ubuntu 24.04 robot images stuck in
    the build queue indefinitely.
    
    Replace facter with /etc/os-release as the primary OS detection
    method with facter as a fallback for legacy images. Add fallback
    logic for locating authorized_keys across common cloud user home
    directories (ubuntu, centos).
    
    Issue: RELENG-5949
    Change-Id: I2d5067ac425020b15e150784cfb7e8548e8af060
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- jenkins-init-scripts/create-jenkins-user.sh
+++ jenkins-init-scripts/create-jenkins-user.sh
@@ -9,8 +9,20 @@
 # http://www.eclipse.org/legal/epl-v10.html
 ##############################################################################
 
-OS=$(facter operatingsystem | tr '[:upper:]' '[:lower:]')
-OS_RELEASE=$(facter lsbdistrelease | tr '[:upper:]' '[:lower:]')
+# Detect OS using /etc/os-release (works on all modern distros)
+# Falls back to facter for legacy images that still have it installed
+if [ -f /etc/os-release ]; then
+    # shellcheck disable=SC1091
+    . /etc/os-release
+    OS=$(echo "$ID" | tr '[:upper:]' '[:lower:]')
+    OS_RELEASE="$VERSION_ID"
+elif command -v facter > /dev/null 2>&1; then
+    OS=$(facter operatingsystem | tr '[:upper:]' '[:lower:]')
+    OS_RELEASE=$(facter lsbdistrelease | tr '[:upper:]' '[:lower:]')
+else
+    echo "ERROR: Unable to detect OS: /etc/os-release not found and facter not installed"
+    exit 1
+fi
 
 if [[ "$OS_RELEASE" == "18.04" && "$OS" == 'ubuntu' ]]; then
     # We do not want var expansion here as profile script expands at runtime.
@@ -30,7 +42,17 @@ if grep -q mock /etc/group; then
 fi
 
 mkdir /home/jenkins/.ssh
-cp "/home/${OS}/.ssh/authorized_keys" /home/jenkins/.ssh/authorized_keys
+# Find the default cloud user's authorized_keys to copy to jenkins user
+if [ -f "/home/${OS}/.ssh/authorized_keys" ]; then
+    cp "/home/${OS}/.ssh/authorized_keys" /home/jenkins/.ssh/authorized_keys
+elif [ -f "/home/ubuntu/.ssh/authorized_keys" ]; then
+    cp /home/ubuntu/.ssh/authorized_keys /home/jenkins/.ssh/authorized_keys
+elif [ -f "/home/centos/.ssh/authorized_keys" ]; then
+    cp /home/centos/.ssh/authorized_keys /home/jenkins/.ssh/authorized_keys
+else
+    echo "ERROR: Unable to find authorized_keys for default cloud user"
+    exit 1
+fi
 chmod 0600 /home/jenkins/.ssh/authorized_keys
 
 # Generate ssh key for use by Robot jobs
